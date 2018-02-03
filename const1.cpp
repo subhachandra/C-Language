d9576fcdcec9f627b791922c057dc44f054d9689
swarnalatha025 <swarnalatha025@gmail.com>
2018-02-03 09:07:42 -0800

    adding new file

---
+++ const1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+ 
+class construct
+{ 
+public: 
+    int a, b;
+         
+        // Default Constructor
+    construct()
+    {
+      cout<< " base class \n"; 
+       a = 10;
+        b = 20;
+    }
+};
+class con1: public construct
+{
+public:
+    int a, b;
+
+        // Default Constructor
+    con1()
+    {
+        a = 10;
+        b = 20;
+    }
+};
+
+ 
+int main()
+{
+        // Default constructor called automatically
+        // when the object is created
+//    construct c;
+ //   cout<< "a: "<< c.a << endl << "b: "<< c.b;
+con1 c1;
+cout<< " \n in c1->a: "<< c1.a << endl << "in c1-->b: "<< c1.b;    
+return 1;
+}
