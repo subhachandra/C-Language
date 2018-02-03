525ef99bfc03d06f652b20e9da9b92487e5a8c7d
swarnalatha025 <34466415+swarnalatha025@users.noreply.github.com>
2018-02-03 22:43:58 +0530

    updated
    file updated

--- const1.cpp
+++ const1.cpp
@@ -32,8 +32,8 @@ int main()
 {
         // Default constructor called automatically
         // when the object is created
-//    construct c;
- //   cout<< "a: "<< c.a << endl << "b: "<< c.b;
+    construct c;
+   cout<< "a: "<< c.a << endl << "b: "<< c.b;
 con1 c1;
 cout<< " \n in c1->a: "<< c1.a << endl << "in c1-->b: "<< c1.b;    
 return 1;
