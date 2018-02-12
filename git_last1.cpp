652ea42a5da612925e1ac18f11d4ef645657efef
subhachandra <chandrapdtr.j2ee@gmail.com>
2018-02-12 22:47:22 +0530

    reverted with program code
    reverted with program code

--- git_last1.cpp
+++ git_last1.cpp
@@ -1,3 +1,24 @@
-write a programs to catch exceptions
+#include <iostream>
+using namespace std;
 
-when catch the exception to handle it /fix it
+double division(int a, int b) {
+   if( b == 0 ) {
+      throw "Division by zero condition!";
+   }
+   return (a/b);
+}
+
+int main () {
+   int x = 50;
+   int y = 0;
+   double z = 0;
+ 
+   try {
+      z = division(x, y);
+      cout << z << endl;
+   } catch (const char* msg) {
+     cerr << msg << endl;
+   }
+
+   return 0;
+}
